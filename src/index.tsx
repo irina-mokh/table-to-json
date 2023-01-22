e6b078d01e7aef0ddf6c277a151028161229aee0
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2023-01-22 11:48:03 +0300

    fix: disable prettier for one line

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './styles/index.scss';
 import { App } from './components/App';
 
 const root = ReactDOM.createRoot(
+  // eslint-disable-next-line prettier/prettier
   document.getElementById('root') as HTMLElement
 );
 root.render(
